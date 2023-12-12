14da09953d45a4758e19979936e09d5c152df9e2
Saifurrahman54 <saifibaba54@gmail.com>
2023-12-11 21:41:52 -0800

    Js-1

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var myName=("My Name is Saif Ur Rahman");
+var wab1= ("Error! Please enter a valid password.");
+var wab2= ("Welcome to the Js.");
+var wab2=("Happy  Coding");
+var wab3=("Welcome to JS Land");
+var js1= ("Happy Coding");
+var js1=("Prevent this page from creating additional dialogs");
+var js2 =("Hello... I can run JS through web browser's console");
+alert(myName)
+alert(wab1)
+alert(wab2)
+alert(wab3)
+alert(js1)
+alert(js2)
